fa2c41f6262a778d6c4606c7d9178f3be8fd81cc
yu-wongyun <wongyun@won-gyun-Mac-mini.local>
2025-07-01 13:45:57 +0900

    Change github username

--- src/site.config.ts
+++ src/site.config.ts
@@ -27,7 +27,7 @@ export const siteConfig: SiteConfig = {
 	*/
 	title: "Wongyun Yu",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://201820894.github.io/",
+	url: "https://wongyun-yu.github.io/",
 };
 
 // Used to generate links in both the Header & Footer.
